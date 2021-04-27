d10ab72eebebdb81873c59533c2a462ab1558a96
Alexandre Rogério <xandaosilva93@hotmail.com>
2021-04-27 16:15:46 -0300

    Develop App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import GithubImage from './github.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">Github profile</h1>
+      <form>
+        <div className="form-group">
+          <div className="input-group">
+            <input type="text" className="form-control" required/>
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        <img src={GithubImage} className="responsive rounded-circle" height="200px" alt="" />
+        <h1 className="pt-5">
+          <a href="https://github.com/xandaosilva" target="_new">
+            Alexandre Rogério
+          </a>
+        </h1>
+        <h3>Uberlândia</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="https://www.facebook.com/alexandre.rogerio.3" target="_new" className="text-info">
+            https://www.facebook.com/alexandre.rogerio.3
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
